ca7edb88ee9100a9edabea6024b506532e7b25e3
Maxime De Greve <maximedegreve@me.com>
2018-03-26 13:01:39 +0200

    Added screenshot

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,11 +7,19 @@
 
 var marvelGraphQL = new MarvelGraphQL("PWszPnfm3aqASM3edc5kWf8fZAoY1jAwJIM3qXWF", "projects:read user:read company.projects:read company:read")
 
+// Timer
+// ------------------------------------------------------------
+// Used for updating the projects every 60 seconds
+
+var timer;
+
 // Start (jQuery)
 // ------------------------------------------------------------
 // To keep it super easy we use jQuery for this little app...
 
 $(document).ready(function(){
+
+
 
 		function checkForTokenInUrl(){
 
@@ -60,10 +68,183 @@ $(document).ready(function(){
 
 		}
 
+		// Find the last updated images
+		// ------------------------------------------------------------
+
+		function findLastUpdateImages(){
+
+		  lastCompanyProjectsPKs(function(PKs, error) {
+
+		    // Check for errors
+		    if (error == Error.NO_PROJECTS){
+		      showError("No projects found in your Marvel account...");
+		      return
+		    } else if (error == Error.NO_COMPANY) {
+		      showError("You have to be on a company plan to use this...");
+		      return
+		    }
+
+		    findImagesForProjectsWithPk(PKs, function(images) {
+
+		      // Sort by date
+		      var imagesSorted = images.sort(function(a, b) {
+		          var dateA = Date.parse(a["modifiedAt"])
+		          var dateB = Date.parse(b["modifiedAt"])
+		          return dateB.compareTo(dateA)
+		      });
+
+		      // Remove the ones without content and example images
+		      var newArray = imagesSorted.filter(function (image) {
+
+		          if (image["content"] == null){
+		            return false
+		          }
+
+		          if (image["content"]["url"] == null){
+		            return false
+		          }
+
+		          if(image["content"]["url"].indexOf("marvelapp.com/static/assets/images/onboarding") != -1){
+		             return false
+		          }
+
+		          return true
+		      });
+
+		      if (newArray.length == 0){
+		        showError("There are no images inside your projects.");
+		        return
+		      }
+
+		      showImages(newArray)
+		      showLoggedIn()
+
+		    });
+
+		  });
+
+		}
+
+		function lastCompanyProjectsPKs(callback, error){
+
+		  $.when(
+		    marvelGraphQL.companyProjects()
+		  ).then(function(projectJson) {
+
+		    // All images found throughout projects
+		    var projectPKs = []
+
+		    // Marvel sorts projects already by last updated...
+		    var company = projectJson["data"]["user"]["company"]
+
+		    // Check if company exists
+		    if (company == null){
+		      callback(null, Error.NO_COMPANY)
+		    }
+
+		    var projects = company["projects"]["edges"]
+
+		    // Check if there are projects
+		    if (projects.length == 0){
+		      callback(null, Error.NO_PROJECTS)
+		    }
+
+		    $.each(projects, function(i, project) {
+
+		      var pk = project["node"]["pk"]
+
+		      if (pk){
+		        projectPKs.push(pk);
+		      }
+
+		    });
+
+		    callback(projectPKs, null)
+
+		  });
+
+		}
+
+		function findImagesForProjectsWithPk(PKs, callback){
+
+		    // All images found throughout projects
+		    var images = []
+
+		    // All calls
+		    var deferreds = [];
+
+		    $.each(PKs, function(i, pk) {
+
+		        deferreds.push(
+
+		          marvelGraphQL.project(pk).then(function(result) {
+
+		            var projectName = result["data"]["project"]["name"]
+		            var nodes = result["data"]["project"]["screens"]["edges"]
+
+		            $.each(nodes, function(i, node) {
+
+		              var dictionary = node["node"];
+		              dictionary["projectName"] = projectName
+		              images.push(dictionary)
+
+		            });
+
+		          })
+
+		        );
+
+		    })
+
+		    $.when.apply($, deferreds).then(function() {
+		      callback(images)
+		    });
+
+		}
+
+		function showImages(images){
+
+		  // Empty
+		  $('#lastUpdated').html("");
+
+		  var first3Images = images.slice(0, 3);
+
+		  $.each(first3Images, function(i, image) {
+
+		      var img = ""
+					img += "<div class='flexItem'>";
+					img += "<div class='centerBox'>";
+					img += "<div class='image'>";
+					img += "<img src='" + image["content"]['url'] + "'>";
+					img += "</div>";
+					img += "<div class='footer'>" + image['projectName'] + "</div>";
+					img += "</div>"
+					img += "</div>"
+
+		      $('#lastUpdated').append(img)
+
+		  });
+
+		}
+
+		// Helpers
+		// ------------------------------------------------------------
+
+		function logout(){
+		  localStorage.removeItem("accessToken")
+		  showLoggedOut()
+		}
+
 		function getParams(url, k){
 		 var p={};
 		 url.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){p[k]=v})
 		 return k?p[k]:p;
+		}
+
+		function setupTimer(){
+		  // Update projects every 60 seconds
+		  clearInterval(timer);
+		  timer = setInterval(findLastUpdateImages, 60000);
 		}
 
 		// States
