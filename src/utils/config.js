bd709e2f2b9afb34ac7ec2b39203908944410c7a
Joe Alcorn <joe.alcorn@marvelapp.com>
2018-06-28 10:38:36 +0100

    remove duplicate scope

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,4 +3,4 @@ export const MarvelUrl = "https://marvelapp.com";
 export const GraphqlEndpoint = "https://api.marvelapp.com/graphql";
 export const ClientId = "7Ig0DiIJJNSFiX8xx6VS9lubcymTqBGgfUYZuzpa";
 export const Scopes =
-  "projects:read user:read company.projects:read company:read company.projects:read";
+  "user:read projects:read company:read company.projects:read";
